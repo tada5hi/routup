b2dc06f1f7cf5a69e84c2c75f3b089025af54a70
tada5hi <tada5hi@users.noreply.github.com>
2022-11-08 14:50:58 +0000

    deploy: b6175da5a005a2f29a7cd8d0420c9fec3ba6dd4f

---
+++ assets/plugins_query_installation.md.9b2ebbad.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as s,a as e}from"./app.be913beb.js";const m=JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"plugins/query/installation.md"}'),n={name:"plugins/query/installation.md"},o=e("",3),l=[o];function i(c,p,r,_,d,h){return s(),t("div",null,l)}const f=a(n,[["render",i]]);export{m as __pageData,f as default};
