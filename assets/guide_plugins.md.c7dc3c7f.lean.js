a1e6da168bc8c07d53fa2b19ded27d9f2d13224f
tada5hi <tada5hi@users.noreply.github.com>
2022-11-11 10:12:00 +0000

    deploy: 6190fbee24b691a0e2662215afd6cabaebaeeaee

--- assets/guide_plugins.md.c7dc3c7f.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a,a as r}from"./app.0e7b2621.js";const g=JSON.parse('{"title":"Plugins","description":"","frontmatter":{},"headers":[],"relativePath":"guide/plugins.md"}'),s={name:"guide/plugins.md"},i=r("",3),o=[i];function d(n,c,p,h,u,l){return a(),e("div",null,o)}const f=t(s,[["render",d]]);export{g as __pageData,f as default};
