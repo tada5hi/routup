03c82d55981ff40ff37c455618a93657759c667a
tada5hi <tada5hi@users.noreply.github.com>
2022-12-21 16:32:17 +0000

    deploy: cd7a91396244a638b08efd1fc23fe4b225baf484

---
+++ assets/guide_plugins.md.57c7169e.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as r}from"./app.68434019.js";const g=JSON.parse('{"title":"Plugins","description":"","frontmatter":{},"headers":[],"relativePath":"guide/plugins.md"}'),s={name:"guide/plugins.md"},d=r("",3),o=[d];function i(n,c,p,h,u,l){return a(),t("div",null,o)}const f=e(s,[["render",i]]);export{g as __pageData,f as default};
---
+++ assets/plugins_static_installation.md.507ac6c2.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o as s,a as n}from"./app.68434019.js";const m=JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"plugins/static/installation.md"}'),e={name:"plugins/static/installation.md"},o=n("",3),i=[o];function l(c,p,r,_,d,h){return s(),t("div",null,i)}const f=a(e,[["render",l]]);export{m as __pageData,f as default};
